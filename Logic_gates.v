c230ae85fb7e75d3b186bc791dcdae71afad9488
Anh Do <36804143+Anh-tp-do@users.noreply.github.com>
2022-06-09 12:36:31 -0500

    Update Logic_gates.v

--- Logic_gates.v
+++ Logic_gates.v
@@ -1,22 +1,34 @@
-module v_nand
+module logic_gates(a,b, out_nand, out_inv, out_and, out_or, out_xor)
+  input wire a, b;
+  output wire out_nand, out_inv, out_and, out_or, out_xor;
+  
+  nand(out_nand, a,b);
+  not(out_inv, a,b);
+  and(out_and, a,b);
+  or(out_or, a,b);
+  xor(out_xor, a,b);
+    
+endmodule 
 
+// Testbench 
+module tb_logicgates;
+  reg a,b;
+  wire out_nand, out_inv, out_and, out_or, out_xor;
+  integer i;
 
-endmdule
-
-//
-
-module v_invert 
-
-endmodule
-
-module v_and 
-
-endmodule
-
-module v_or 
-
-endmodule
-
-module v_xor 
-
+  logic_gates u0 (.a(a), .b(b), .out_nand(out_nand), .out_inv(out_inv), .out_and(out_and), .out_or(out_or), .out_xor(out_xor);
+  
+  initial 
+    begin
+      {a,b} = 0
+      
+      $monitor( 
+        for (i = 0; i < 4; i = i+1)
+          begin
+            #1 a <= $random;
+               b <= $random;
+          end
+      
+    end 
+  
 endmodule
